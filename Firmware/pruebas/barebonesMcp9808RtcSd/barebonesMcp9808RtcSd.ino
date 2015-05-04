c6df7f9b4ff7629967d04357d38035808578b82e
Rene Tapia <rtapiapincheira@gmail.com>
2015-05-04 01:23:04 -0400

    Trabajo en progreso (don't merge into master). Se actualiza librería de RTC y se crea prueba minima para ejecutar sd+rtc+mcp9808.
    La librería de RTC actualizada desde Adafruit introduce cambios mayores y requiere ser adaptado el código de elibatt.

---
+++ Firmware/pruebas/barebonesMcp9808RtcSd/barebonesMcp9808RtcSd.ino
@@ -0,0 +1,117 @@
+
+#include <SPI.h>
+#include <SD.h>
+#include <Wire.h>
+
+#include "Adafruit_MCP9808.h"
+#include <RTClib.h>
+
+// Create the MCP9808 temperature sensor object
+Adafruit_MCP9808 tempsensor = Adafruit_MCP9808();
+RTC_Millis RTC;
+File myFile;
+
+void setup() {
+  Serial.begin(57600);
+  Serial.println("MCP9808 demo");
+  
+  // Make sure the sensor is found, you can also pass in a different i2c
+  // address with tempsensor.begin(0x19) for example
+  if (!tempsensor.begin()) {
+    Serial.println("Couldn't find MCP9808!");
+    while (1);
+  }
+
+  // -------------------------------
+
+  // following line sets the RTC to the date & time this sketch was compiled
+  //DateTime sketch(__DATE__, __TIME__);
+  //RTC.begin(DateTime (sketch.unixtime() + 20));
+  
+  // -------------------------------
+
+  Serial.print("Initializing SD card...");
+  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
+  // Note that even if it's not used as the CS pin, the hardware SS pin
+  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
+  // or the SD library functions will not work.
+  pinMode(10, OUTPUT);
+
+  if (!SD.begin(38)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop() {
+  Serial.println("Getting temperature");
+  
+  // Read and print out the temperature, then convert to *F
+  float c = 0.0;//tempsensor.readTempC();
+  float f = c * 9.0 / 5.0 + 32;
+  Serial.print("Temp: "); Serial.print(c); Serial.print("*C\t"); 
+  Serial.print(f); Serial.println("*F");
+  
+  Serial.println("Getting time");
+  
+  // -------------------------------
+  
+  DateTime now = RTC.now();
+    
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(' ');
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+    
+    Serial.print(" seconds since 1970: ");
+    Serial.println(now.unixtime());
+    
+    Serial.println();
+  
+  // -------------------------------
+  
+  Serial.println("Waiting some time");
+  
+  delay(2500);
+}
