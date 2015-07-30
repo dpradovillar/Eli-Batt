6976ce507641f7be31848d9085e913c345d3d293
Rene Tapia <rtapiapincheira@gmail.com>
2015-07-29 23:48:38 -0300

    Agrega marco de primer grafico.

--- Software/Elibatt-Viewer/reportgenerator.cpp
+++ Software/Elibatt-Viewer/reportgenerator.cpp
@@ -4,7 +4,6 @@
 #include <QApplication>
 #include <QTextDocument>
 
-
 ReportGenerator::ReportGenerator(const QString &inputFilename, const QString &outputFilename) {
     m_inputFilename = inputFilename;
     m_outputFilename = outputFilename;
@@ -22,16 +21,17 @@ QString ReportGenerator::create() {
 
     // Header
     _setBrush(0x231F20);
-    _fillRect(0, 10, 215.9, 30);
-    _drawPixmap(170, 10, 30, 30, "res/logo_64.png");
+    QRectF hRect(0, 10, 215.9, 30);
+    _fillRect(hRect);
+    _drawPixmap(QRectF(170, 10, 30, 30), "res/logo_64.png");
 
     _setPen(0xFFFFFF);
-    p.setFont(QFont("Helvetica", 36));
-    _drawText(25, 30, "Reporte E-LiBatt");
+    _setFont(36);
+    _drawText(hRect, "Reporte E-LiBatt", Qt::AlignCenter);
 
     // Body
     _setPen(0x000000);
-    p.setFont(QFont("Helvetica", 12));
+    _setFont(12);
     QVector<QStringList> d = QVector<QStringList>()
         << (QStringList() << "Período"                << ":" << "dd/mm/yy a dd/mm/yy")
         << (QStringList() << "Número de muestras"     << ":" << "86400")
@@ -44,17 +44,28 @@ QString ReportGenerator::create() {
         d
     );
 
-    p.end();
+    _setPen(0x000000);
+    _setFont(10);
+    _drawFrameGraph(QRectF(25, 90, 165.9, 50),
+        QList<double>() << 0.0 << 10.0 << 20.0 << 30.0 << 40,
+        QList<QDateTime>() <<
+            QDateTime::fromMSecsSinceEpoch(86400*1000) <<
+            QDateTime::fromMSecsSinceEpoch(86400*2000) <<
+            QDateTime::fromMSecsSinceEpoch(86400*3000) <<
+            QDateTime::fromMSecsSinceEpoch(86400*4000) <<
+            QDateTime::fromMSecsSinceEpoch(86400*5000)
+    );
 
+    p.end();
     return "";
 }
 
 int ReportGenerator::convertX(qreal x_mm) {
-    const qreal wPage = 2550.0;
+    static const qreal wPage = (300.0 * 8.5);
     return (int)(x_mm * wPage / 215.9);
 }
 int ReportGenerator::convertY(qreal y_mm) {
-    const qreal hPage = 3300.0;
+    static const qreal hPage = (300.0 * 11.0);
     return (int)(y_mm * hPage / 279.4);
 }
 
@@ -68,6 +79,9 @@ quint32 ReportGenerator::convertH(qreal height_mm) {
 QPoint ReportGenerator::convert(qreal x_mm, qreal y_mm) {
     return QPoint(convertX(x_mm), convertY(y_mm));
 }
+QRectF ReportGenerator::convert(const QRectF &r) {
+    return QRectF(convertX(r.x()), convertY(r.y()), convertW(r.width()), convertH(r.height()));
+}
 
 void ReportGenerator::_setPen(int hex) {
     m_printer->setPen(QColor(QRgb(hex)));
@@ -75,15 +89,29 @@ void ReportGenerator::_setPen(int hex) {
 void ReportGenerator::_setBrush(int hex) {
     m_printer->setBrush(QColor(QRgb(hex)));
 }
-void ReportGenerator::_fillRect(double x_mm, double y_mm, double w_mm, double h_mm) {
-    m_printer->fillRect(convertX(x_mm), convertY(y_mm), convertW(w_mm), convertH(h_mm), m_printer->brush());
+void ReportGenerator::_setFont(int size, const QString &fontName) {
+    m_printer->setFont(QFont(fontName, size));
 }
-void ReportGenerator::_drawPixmap(double x_mm, double y_mm, double w_mm, double h_mm, const QString &resourceName) {
-    m_printer->drawPixmap(convertX(x_mm), convertY(y_mm), convertW(w_mm), convertH(h_mm), QPixmap(resourceName));
+void ReportGenerator::_fillRect(const QRectF &bounds_mm) {
+    m_printer->fillRect(convertX(bounds_mm.x()), convertY(bounds_mm.y()), convertW(bounds_mm.width()), convertH(bounds_mm.height()), m_printer->brush());
 }
-void ReportGenerator::_drawText(double x_mm, double y_mm, const QString &text) {
-    m_printer->drawText(convertX(x_mm), convertY(y_mm), text);
+void ReportGenerator::_drawRect(const QRectF &bounds_mm) {
+    QBrush q = m_printer->brush();
+    m_printer->setBrush(Qt::NoBrush);
+    m_printer->drawRect(convertX(bounds_mm.x()), convertY(bounds_mm.y()), convertH(bounds_mm.width()), convertH(bounds_mm.height()));
+    m_printer->setBrush(q);
 }
+void ReportGenerator::_drawPixmap(const QRectF &bounds_mm, const QString &resourceName) {
+    m_printer->drawPixmap(convertX(bounds_mm.x()), convertY(bounds_mm.y()), convertW(bounds_mm.width()), convertH(bounds_mm.height()), QPixmap(resourceName));
+}
+void ReportGenerator::_drawText(const QRectF& boundingBox, const QString &text, int flags) {
+    m_printer->drawText(convert(boundingBox), flags, text);
+}
+
+void ReportGenerator::_drawLine(double x1_mm, double y1_mm, double x2_mm, double y2_mm) {
+    m_printer->drawLine(QLineF(convertX(x1_mm), convertY(y1_mm), convertX(x2_mm), convertY(y2_mm)));
+}
+
 void ReportGenerator::_drawTable(double x_mm, double y_mm, const QList<double> &colWidths, double cellHeight, const QVector<QStringList> &rows) {
     int numRows = rows.size();
     int numCols = (numRows > 0 ? rows[0].size() : 0);
@@ -101,9 +129,64 @@ void ReportGenerator::_drawTable(double x_mm, double y_mm, const QList<double> &
     }
     double xGap = 0.0;
     for (int i = 0; i < numCols; i++) {
+        qreal wi = colWidths[i];
         for (int j = 0; j < numRows; j++) {
-            _drawText(x_mm+xGap, y_mm+j*cellHeight, rows[j][i]);
+            _drawText(QRectF(x_mm+xGap, y_mm+j*cellHeight, wi, cellHeight), rows[j][i]);
         }
-        xGap += colWidths[i];
+        xGap += wi;
     }
+}
+
+void ReportGenerator::_drawFrameGraph(const QRectF &bounds_mm, const QList<double> &yLabels, const QList<QDateTime> &xLabels) {
+    qDebug() << "Invalid QRectF values:"
+        << bounds_mm.x() << "," << bounds_mm.y()
+        << " - "
+        << bounds_mm.width() << "x" << bounds_mm.height()
+    ;
+    if (bounds_mm.x() < 0 || bounds_mm.y() < 0 || bounds_mm.width() == 0 || bounds_mm.height() == 0) {
+        return;
+    }
+    _drawRect(bounds_mm);
+
+    static const QString d_format1 = "yyyy-MM-dd";
+    static const QString d_format2 = "hh:mm:ss";
+
+    if (yLabels.size() > 1) {
+        static const qreal y_tick_length = 1.0;
+        static const qreal bb_h = 4.0;
+        static const qreal bb_w = 10.0;
+
+        qreal bb_x = bounds_mm.x();
+        for (int i = 0; i < yLabels.size(); i++) {
+            qreal bb_y = (bounds_mm.height() + bounds_mm.y()) - (bounds_mm.height() * i) / (yLabels.size() - 1);
+            _drawLine(bb_x - y_tick_length, bb_y, bb_x, bb_y);
+
+            QRectF rect(bb_x - bb_w - y_tick_length - 0.5, bb_y - bb_h / 2.0, bb_w, bb_h);
+            _drawText(rect, QString::number(yLabels[i], 'f', 1), Qt::AlignVCenter | Qt::AlignRight);
+            //_drawRect(rect);
+        }
+    }
+
+    if (xLabels.size() > 1) {
+        static const qreal x_tick_length = 1.0;
+
+        qreal wGap = bounds_mm.width() / (xLabels.size() - 1);
+        qreal bb_y = bounds_mm.y() + bounds_mm.height();
+        qreal bb_h = 4.0;
+        for (int i = 0; i < xLabels.size(); i++) {
+            qreal bb_x = bounds_mm.x() + i * wGap ;
+            _drawLine(bb_x, bb_y, bb_x, bb_y + x_tick_length);
+
+            QRectF rect1(bb_x - wGap / 2.0, bb_y + bb_h + x_tick_length + 0.5, wGap, bb_h);
+            _drawText(rect1, xLabels[i].toString(d_format1), Qt::AlignCenter);
+            //_drawRect(rect1);
+
+            QRectF rect2(bb_x - wGap / 2.0, bb_y + x_tick_length + 0.5, wGap, bb_h);
+            _drawText(rect2, xLabels[i].toString(d_format2), Qt::AlignCenter);
+            //_drawRect(rect2);
+        }
+    }
+}
+void ReportGenerator::_drawPlotGraph(const QRectF &bounds_mm, const QList<double> &yValues, const QList<QDateTime> &xValues) {
+
 }
--- Software/Elibatt-Viewer/reportgenerator.h
+++ Software/Elibatt-Viewer/reportgenerator.h
@@ -24,13 +24,21 @@ public:
     quint32 convertH(qreal height_mm);
 
     QPoint convert(qreal x_mm, qreal y_mm);
+    QRectF convert(const QRectF &r);
 
     void _setPen(int hex);
     void _setBrush(int hex);
-    void _fillRect(double x_mm, double y_mm, double w_mm, double h_mm);
-    void _drawPixmap(double x_mm, double y_mm, double w_mm, double h_mm, const QString &resourceName);
-    void _drawText(double x_mm, double y_mm, const QString &text);
+    void _setFont(int size, const QString &fontName = "Helvetica");
+    void _fillRect(const QRectF &bounds_mm);
+    void _drawRect(const QRectF &bounds_mm);
+    void _drawPixmap(const QRectF &bounds_mm, const QString &resourceName);
+    void _drawText(const QRectF& boundingBox, const QString &text, int flags = 0);
+
+    void _drawLine(double x1_mm, double y1_mm, double x2_mm, double y2_mm);
+
     void _drawTable(double x_mm, double y_mm, const QList<double> &colWidths, double cellHeight, const QVector<QStringList> &rows);
+    void _drawFrameGraph(const QRectF &bounds_mm, const QList<double> &yLabels, const QList<QDateTime> &xLabels);
+    void _drawPlotGraph(const QRectF &bounds_mm, const QList<double> &yValues, const QList<QDateTime> &xValues);
 };
 
 #endif // REPORT_GENERATOR_H
